d85858aabf0aeea5547df2b4027f0bbdfedecc29
billywiltshire77 <billywiltshire77@gmail.com>
2022-04-12 19:50:41 -0500

    Savepoint: Map built out and markers made. Pending styling and cleanup.

--- Leaflet-Step-1/plot.js
+++ Leaflet-Step-1/plot.js
@@ -7,15 +7,64 @@ d3.json(url).then((data) => {
 function configureMap(earthquakeData) {
     console.log(earthquakeData)
 
+    function markerSize(felt) {
+        return Math.sqrt(felt) * 100000;
+    }
+
     function onEachEarthquake(feature, layer) {
         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-      }
+        
+        quakeMarkers = []
+        for (let quake of earthquakeData) {
+        if (quake.properties.mag > 7) {
+            quakeMarkers.push(
+                L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
+                stroke: false,
+                fillOpacity: 0.75,
+                color: "black",
+                fillColor: "red",
+                radius: markerSize(quake.properties.felt)
+                }).bindPopup('Popup')
+            );
+        } else if (quake.properties.mag > 5) {
+            quakeMarkers.push(
+                L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
+                  stroke: false,
+                  fillOpacity: 0.75,
+                  color: "black",
+                  fillColor: "orange",
+                  radius: markerSize(quake.properties.felt)
+                }).bindPopup('Popup')
+            );
+        } else if (quake.properties.mag > 3) {
+            quakeMarkers.push(
+                L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
+                  stroke: false,
+                  fillOpacity: 0.75,
+                  color: "black",
+                  fillColor: "yellow",
+                  radius: markerSize(quake.properties.felt)
+                }).bindPopup('Popup')
+            );
+        } else {
+            quakeMarkers.push(
+                L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
+                  stroke: false,
+                  fillOpacity: 0.75,
+                  color: "black",
+                  fillColor: "green",
+                  radius: markerSize(quake.properties.felt)
+                }).bindPopup('Popup')
+            );
+        }
+        }
+    }
 
-      var earthquakes = L.geoJSON(earthquakeData, {
+    let earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachEarthquake
-      });
+    });
 
-      createMap(earthquakes);
+    createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
@@ -28,9 +77,12 @@ function createMap(earthquakes) {
         "Street Map": street,
       };
     
-      // Create an overlay object to hold our overlay.
+    console.log(quakeMarkers)
+    let quakes = L.layerGroup(quakeMarkers);
+
     let overlayMaps = {
-        Earthquakes: earthquakes
+        Earthquakes: earthquakes,
+        Size: quakes
       };
 
     let myMap = L.map("map", {
@@ -38,7 +90,7 @@ function createMap(earthquakes) {
           0,0
         ],
         zoom: 1,
-        layers: [street, earthquakes]
+        layers: [street, earthquakes, quakes]
     });
 
     L.control.layers(baseMaps, overlayMaps, {
