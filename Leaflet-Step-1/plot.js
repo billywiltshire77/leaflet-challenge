373b0c11ed5c8790ea468a5d88b50ba03144f199
billywiltshire77 <billywiltshire77@gmail.com>
2022-04-14 19:22:19 -0500

    Part 1 Done except legend

--- Leaflet-Step-1/plot.js
+++ Leaflet-Step-1/plot.js
@@ -1,4 +1,4 @@
-let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
 
 d3.json(url).then((data) => {
     configureMap(data.features)
@@ -8,11 +8,10 @@ function configureMap(earthquakeData) {
     console.log(earthquakeData)
 
     function markerSize(felt) {
-        return Math.sqrt(felt) * 100000;
+        return Math.sqrt(felt) * 1000;
     }
 
     function onEachEarthquake(feature, layer) {
-        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
         
         quakeMarkers = []
         for (let quake of earthquakeData) {
@@ -24,7 +23,7 @@ function configureMap(earthquakeData) {
                 color: "black",
                 fillColor: "red",
                 radius: markerSize(quake.properties.felt)
-                }).bindPopup('Popup')
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
             );
         } else if (quake.properties.mag > 5) {
             quakeMarkers.push(
@@ -34,7 +33,7 @@ function configureMap(earthquakeData) {
                   color: "black",
                   fillColor: "orange",
                   radius: markerSize(quake.properties.felt)
-                }).bindPopup('Popup')
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
             );
         } else if (quake.properties.mag > 3) {
             quakeMarkers.push(
@@ -44,7 +43,7 @@ function configureMap(earthquakeData) {
                   color: "black",
                   fillColor: "yellow",
                   radius: markerSize(quake.properties.felt)
-                }).bindPopup('Popup')
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
             );
         } else {
             quakeMarkers.push(
@@ -54,7 +53,7 @@ function configureMap(earthquakeData) {
                   color: "black",
                   fillColor: "green",
                   radius: markerSize(quake.properties.felt)
-                }).bindPopup('Popup')
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
             );
         }
         }
@@ -81,19 +80,43 @@ function createMap(earthquakes) {
     let quakes = L.layerGroup(quakeMarkers);
 
     let overlayMaps = {
-        Earthquakes: earthquakes,
-        Size: quakes
+        Earthquakes: quakes,
       };
 
     let myMap = L.map("map", {
         center: [
           0,0
         ],
-        zoom: 1,
-        layers: [street, earthquakes, quakes]
+        zoom: 2,
+        layers: [street, quakes]
     });
 
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
 }
+// Started Legend Code, needs to be finished.
+// let legend = L.control({ position: "bottomright" });
+//   legend.onAdd = function() {
+//     let div = L.DomUtil.create("div", "info legend");
+//     let labels = [];
+//     let categories = ["Very Strong", "Strong", "Medium", "Weak"];
+
+//     // Add the minimum and maximum.
+//     let legendInfo = ["<h1>Legend</h1>"];
+//     for (let category of categories) {
+//         legendInfo.push(`<ul>${category}</ul>`)
+//     }
+
+//     div.innerHTML = legendInfo;
+
+//     limits.forEach(function(limit, index) {
+//       labels.push("<li style=\"background-color: " +  + "\"></li>");
+//     });
+
+//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+//     return div;
+//   };
+
+//   // Adding the legend to the map
+//   legend.addTo(myMap);
