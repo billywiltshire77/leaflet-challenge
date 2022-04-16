0658eabf2487f587ecc05820962f9c60fe27f6bf
billywiltshire77 <billywiltshire77@gmail.com>
2022-04-15 19:41:30 -0500

    Earthquake markers complete, pending legend completion

--- Leaflet-Step-1/plot.js
+++ Leaflet-Step-1/plot.js
@@ -1,49 +1,58 @@
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson";
 
 d3.json(url).then((data) => {
-    configureMap(data.features)
+    configureMap(data)
 })
 
-function configureMap(earthquakeData) {
-    console.log(earthquakeData)
+function configureMap(data) {
+    console.log(data)
 
-    function markerSize(felt) {
-        return Math.sqrt(felt) * 1000;
+    earthquakeData = data.features
+    maxDepth = data.bbox[5]
+    numBins = [1, 2, 3, 4]
+    depthBins = []
+
+    for (let bin of numBins) {
+        depthBins.push(maxDepth * (bin/numBins.length))
+    }
+
+    function markerSize(mag) {
+        return Math.sqrt(mag) * 10000;
     }
 
     function onEachEarthquake(feature, layer) {
         
         quakeMarkers = []
         for (let quake of earthquakeData) {
-        if (quake.properties.mag > 7) {
+        if (quake.geometry.coordinates[2] < depthBins[0]) {
             quakeMarkers.push(
                 L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
                 stroke: false,
                 fillOpacity: 0.75,
                 color: "black",
-                fillColor: "red",
-                radius: markerSize(quake.properties.felt)
-                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
+                fillColor: "green",
+                radius: markerSize(quake.properties.mag)
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.geometry.coordinates[2]} Meters Deep</p>`)
             );
-        } else if (quake.properties.mag > 5) {
-            quakeMarkers.push(
-                L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
-                  stroke: false,
-                  fillOpacity: 0.75,
-                  color: "black",
-                  fillColor: "orange",
-                  radius: markerSize(quake.properties.felt)
-                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
-            );
-        } else if (quake.properties.mag > 3) {
+        } else if (quake.geometry.coordinates[2] < depthBins[1]) {
             quakeMarkers.push(
                 L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
                   stroke: false,
                   fillOpacity: 0.75,
                   color: "black",
                   fillColor: "yellow",
-                  radius: markerSize(quake.properties.felt)
-                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
+                  radius: markerSize(quake.properties.mag)
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.geometry.coordinates[2]} Meters Deep</p>`)
+            );
+        } else if (quake.geometry.coordinates[2] < depthBins[2]) {
+            quakeMarkers.push(
+                L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
+                  stroke: false,
+                  fillOpacity: 0.75,
+                  color: "black",
+                  fillColor: "orange",
+                  radius: markerSize(quake.properties.mag)
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.geometry.coordinates[2]} Meters Deep</p>`)
             );
         } else {
             quakeMarkers.push(
@@ -51,9 +60,9 @@ function configureMap(earthquakeData) {
                   stroke: false,
                   fillOpacity: 0.75,
                   color: "black",
-                  fillColor: "green",
-                  radius: markerSize(quake.properties.felt)
-                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>Felt By: ${quake.properties.felt} People</p>`)
+                  fillColor: "red",
+                  radius: markerSize(quake.properties.mag)
+                }).bindPopup(`<h3> ${quake.properties.title}</h3><hr><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.properties.type}</p><p>Magnitude: ${quake.properties.mag}</p><p>${quake.geometry.coordinates[2]} Meters Deep</p>`)
             );
         }
         }
@@ -76,7 +85,6 @@ function createMap(earthquakes) {
         "Street Map": street,
       };
     
-    console.log(quakeMarkers)
     let quakes = L.layerGroup(quakeMarkers);
 
     let overlayMaps = {
@@ -94,29 +102,30 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "info legend");
+        let limits = depthBins;
+        let colors = ["green", "yellow", "orange", "red"];
+        let labels = [];
+
+        // Add the minimum and maximum.
+        let legendInfo = "<h1>Legend</h1>" +
+        "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>"
+
+        div.innerHTML = legendInfo;
+
+         limits.forEach(function(limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+         });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    legend.addTo(myMap);
 }
-// Started Legend Code, needs to be finished.
-// let legend = L.control({ position: "bottomright" });
-//   legend.onAdd = function() {
-//     let div = L.DomUtil.create("div", "info legend");
-//     let labels = [];
-//     let categories = ["Very Strong", "Strong", "Medium", "Weak"];
-
-//     // Add the minimum and maximum.
-//     let legendInfo = ["<h1>Legend</h1>"];
-//     for (let category of categories) {
-//         legendInfo.push(`<ul>${category}</ul>`)
-//     }
-
-//     div.innerHTML = legendInfo;
-
-//     limits.forEach(function(limit, index) {
-//       labels.push("<li style=\"background-color: " +  + "\"></li>");
-//     });
-
-//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-//     return div;
-//   };
-
-//   // Adding the legend to the map
-//   legend.addTo(myMap);
